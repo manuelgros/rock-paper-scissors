7c0332f912bcf24922a2b8c5323bc516b732c7d8
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-06-18 11:19:44 -0700

    Create game() and adjust playRound()
    I created the game() which will start a new game of rock, paper,
    scissors. At the start of the game the value for playerLife and
    computerLife should be set to 5. The while loop should now run
    playRounds(), determine a winner or tie after each run and subtract
    1 of the life value of the loser. This should be repeated until the
    playerLife or computerLife reaches zero, which would end the game.
    
    Since the values for playerSelection and computerSelection should be
    declared new each round, I moved the declaration inside the playRound().
    
    For the game() to track the result of each playRound(), I changed the
    function return to a more useful variable.

--- javascript.js
+++ javascript.js
@@ -5,32 +5,58 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-const playerSelection = prompt('Choose your weapon!').toLowerCase()
-const computerSelection = getComputerChoice()
-
-   console.log(playerSelection);
-   console.log(computerSelection);
-
 function playRound(playerSelection, computerSelection) {
+
+  const playerSelection = prompt('Choose your weapon!').toLowerCase()
+  const computerSelection = getComputerChoice()
+
   if (playerSelection === 'rock' && computerSelection === 'rock'){
-    return "It's a tie! Try again";
+    return tie;
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return "You lose! Paper beats Rock";
+    return computerSelection;
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return "You win! Rock beats Scissors";
+    return playerSelection;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return "You win! Paper beats Rock";
+    return playerSelection;
   } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-    return "It's a tie! Try again";
+    return tie;
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return "You lose! Scissors beats Paper";
+    return computerSelection;
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    return "You lose! Rock beats Scissors";
+    return computerSelection;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return "You win! Scissors beats Paper";
+    return playerSelection;
   } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-    return "It's a tie! Try again"
+    return tie
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+
+  // set playerLife to 5
+  let playerLife = 5;
+  // set computerLife to 5
+  let computerLife = 5;
+
+  // start first round
+  while (playerLife > 0 || computerLife > 0) {
+    playRound(playerSelection, computerSelection);
+    // set loserLife to (*Life -1)
+    if (playRound(playerSelection, computerSelection) = playerSelection) {
+      playerLife -= 1;
+      console.log(`You've won! Player: ${playerLife} | Computer: ${computerLife}`)
+    } else if (playRound(playerSelection, computerSelection) = computerSelection) {
+      computerLife -= 1;
+      console.log(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`)
+    } else {
+      console.log(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`)
+    }
+  // start next round - repeat until playerLife or computerLife reaches 0 
+  }
+  // Declare winner of Game
+  if (playerLife == 0) {
+    console.log("The computer won, as you knew he would! - Game Over")
+  } else {
+    console.log("Hurray! Once again the human brain conquered the machine! - Game Over")
+  }
+}
