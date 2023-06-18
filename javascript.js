07cc5c64d99739770c84356291f5fb5c6524bd10
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-06-18 10:20:27 -0700

    tCreate function to determine winner
    I created the function singleRound() to determine the winner
    by comparing the choices through various if statements. However,
    currently the return doesn't seem to match the inputs given by
    playerChoice and computerChoice.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,37 @@
 
 function getComputerChoice() {
-  const choice = ['Rock', 'Paper', 'Scissors']
+  const choice = ['rock', 'paper', 'scissors']
   let computerChoice = choice[Math.floor(Math.random() *choice.length)];
   return computerChoice;
 }
 
 console.log(getComputerChoice())
+
+const playerSelection = prompt('Choose your weapon!').toLowerCase()
+const computerSelection = getComputerChoice()
+
+   console.log(playerSelection);
+
+function singleRound(playerSelection, computerSelection) {
+  if (playerSelection === 'rock' && computerSelection === 'rock'){
+    return "It's a tie! Try again";
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    return "It's a tie! Try again";
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    return "You lose! Scissors beats Paper";
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    return "You lose! Rock beats Scissors";
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return "You win! Scissors beats Paper";
+  } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+    return "It's a tie! Try again"
+  }
+}
+
+console.log(singleRound(playerSelection, computerSelection));
