d7a9c95e2d952d5146fc3146630b4311cad67f23
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-06-18 14:51:26 -0700

    Do minor format changes

--- javascript.js
+++ javascript.js
@@ -9,23 +9,23 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === 'rock' && computerSelection === 'rock'){
-    return "tie";
+      return "tie";
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return "computerWin";
+      return "computerWin";
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return "playerWin";
+      return "playerWin";
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return "playerWin";
+      return "playerWin";
   } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-    return "tie";
+      return "tie";
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return "computerWin";
+      return "computerWin";
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    return "computerWin";
+      return "computerWin";
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return "playerWin";
+      return "playerWin";
   } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-    return "tie";
+      return "tie";
   }
 }
 
@@ -43,27 +43,27 @@ function game() {
     playRound(playerSelection, computerSelection);
       // set loserLife = loserLife -1...
       if (playRound(playerSelection, computerSelection) == "playerWin") {
-        computerLife -= 1;
-        console.log(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
-        alert(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
+          computerLife -= 1;
+          console.log(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
+          alert(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
       } else if (playRound(playerSelection, computerSelection) == "computerWin") {
-        playerLife -= 1;
-        console.log(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
-        alert(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
+          playerLife -= 1;
+          console.log(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
+          alert(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
       // ...or if no one wins, leave Life as is and start new round
       } else {
-        console.log(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`);
-        alert(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`)
+          console.log(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`);
+          alert(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`)
       }
   // start next round - repeat until playerLife or computerLife reaches 0 
   }
 
   // Declare winner of Game
   if (playerLife == 0) {
-    console.log("The computer won, as you knew he would! - Game Over")
-    alert("The computer won, as you knew he would! - Game Over");
+      console.log("The computer won, as you knew he would! - Game Over")
+      alert("The computer won, as you knew he would! - Game Over");
   } else {
-    console.log("Hurray! Once again the human brain conquered the machine! - Game Over")
-    alert("Hurray! Once again the human brain conquered the machine! - Game Over")
+      console.log("Hurray! Once again the human brain conquered the machine! - Game Over")
+      alert("Hurray! Once again the human brain conquered the machine! - Game Over")
   }
 }
