c2118b3b520e9026ba8ed15c0d7ebe70bc171645
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-06-18 15:00:17 -0700

    Do minor changes on comment structure

--- javascript.js
+++ javascript.js
@@ -1,13 +1,11 @@
-// Let 
-function getComputerChoice() {
-  // list 
+function getComputerChoice() { 
   const choices = ['rock', 'paper', 'scissors']
   const computerChoice = choices[Math.floor(Math.random() *choices.length)];
   return computerChoice;
 }
 
+// Run a SINGLE ROUND and determine outcome 
 function playRound(playerSelection, computerSelection) {
-
   if (playerSelection === 'rock' && computerSelection === 'rock'){
       return "tie";
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
@@ -29,36 +27,32 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+// Determine START and END of game and keep track of points
 function game() {
-  // set playerLife and computerLife to 5
   let playerLife = 5;
   let computerLife = 5;
 
-  // start first round
   while (playerLife > 0 && computerLife > 0) {
-  // set value for playerSelection and computerSelection
-  const playerSelection = prompt('Choose your weapon! Pick from: Rock, Paper, or Scissors').toLowerCase();
-  const computerSelection = getComputerChoice();
+    const playerSelection = prompt('Choose your weapon! Pick from: Rock, Paper, or Scissors').toLowerCase();
+    const computerSelection = getComputerChoice();
 
     playRound(playerSelection, computerSelection);
-      // set loserLife = loserLife -1...
-      if (playRound(playerSelection, computerSelection) == "playerWin") {
-          computerLife -= 1;
-          console.log(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
-          alert(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
-      } else if (playRound(playerSelection, computerSelection) == "computerWin") {
-          playerLife -= 1;
-          console.log(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
-          alert(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
-      // ...or if no one wins, leave Life as is and start new round
-      } else {
-          console.log(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`);
-          alert(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`)
-      }
-  // start next round - repeat until playerLife or computerLife reaches 0 
+
+    if (playRound(playerSelection, computerSelection) == "playerWin") {
+        computerLife -= 1;
+        console.log(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
+        alert(`You've won! Player: ${playerLife} | Computer: ${computerLife}`);
+    } else if (playRound(playerSelection, computerSelection) == "computerWin") {
+        playerLife -= 1;
+        console.log(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
+        alert(`You've lost! Player: ${playerLife} | Computer: ${computerLife}`);
+    } else {
+        console.log(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`);
+        alert(`That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`)
+    }
   }
 
-  // Declare winner of Game
+  // Declare WINNER of game
   if (playerLife == 0) {
       console.log("The computer won, as you knew he would! - Game Over")
       alert("The computer won, as you knew he would! - Game Over");
