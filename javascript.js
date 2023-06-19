7f0a3dc8c67f2e6f24dc2cb29c96d4e33be79643
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-06-19 12:09:41 -0700

    Thin out ternary operator and minor changes
    I summed up all conditions that lead to result playerWin with OR
    statements in playRound().
    
    I also added the strings that are being thrown, declaring the winner
    of each round, because the lines were breaking weirdly.

--- javascript.js
+++ javascript.js
@@ -7,9 +7,9 @@ function getComputerChoice() {
 // Run SINGLE ROUND and determine outcome 
 function playRound(playerSelection, computerSelection) {
   return (playerSelection === computerSelection) ? "tie"
-    : (playerSelection === 'rock' && computerSelection === 'scissors') ? "playerWin"
-    : (playerSelection === 'scissors' && computerSelection === 'paper') ? "playerWin"
-    : (playerSelection === 'paper' && computerSelection === 'rock') ? "playerWin"
+    : (playerSelection === 'rock' && computerSelection === 'scissors' ||
+       playerSelection === 'scissors' && computerSelection === 'paper' ||
+       playerSelection === 'paper' && computerSelection === 'rock') ? "playerWin"
     : "computerWin";
 }
 
@@ -26,20 +26,22 @@ function game() {
 
     if (playRound(playerSelection, computerSelection) == "playerWin") {
         computerLife -= 1;
-        let msg = `You've won! ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)} beats 
-                  ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}. 
-                  Player: ${playerLife} | Computer: ${computerLife}`;
+        let msg = `You've won! ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}` +
+                  ` beats ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}.` +
+                  ` Player: ${playerLife} | Computer: ${computerLife}`;
         console.log(msg);
         alert(msg);
     } else if (playRound(playerSelection, computerSelection) == "computerWin") {
         playerLife -= 1;
-        let msg = `You've lost! ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)} beats 
-                  ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}. 
-                  Player: ${playerLife} | Computer: ${computerLife}`;
+        let msg = `You've lost! ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}` +
+                  ` beats ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}.` + 
+                  ` Player: ${playerLife} | Computer: ${computerLife}`;
         console.log(msg);
         alert(msg);
     } else {
         let msg = `That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`
+        console.log(msg);
+        alert(msg);
     }
   }
 
