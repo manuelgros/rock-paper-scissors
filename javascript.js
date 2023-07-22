0381ee01953965d2c31817a7d30b6ff1015fd9a9
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-07-21 19:57:41 -0700

    Change point system and depiction
    I changed points to start from 0 and counting up instead of down (first
    at five wins). The current pints will be represented in the UI and
    should change as the game progresses.

--- javascript.js
+++ javascript.js
@@ -15,29 +15,33 @@ function playRound(playerSelection, computerSelection) {
 
 // Determine START and END of game and keep track of points
 function game() {
-  let playerLife = 5;
-  let computerLife = 5;
+  let playerLife = 0;
+  let computerLife = 0;
 
-  while (playerLife > 0 && computerLife > 0) {
+  while (playerLife < 5 && computerLife < 5) {
     const playerSelection = prompt('Choose your weapon! Pick from: Rock, Paper, or Scissors').toLowerCase();
     const computerSelection = getComputerChoice();
+    const playerP = document.querySelector("#playerP");
+    const computerP = document.querySelector("#computerP");
 
     playRound(playerSelection, computerSelection);
 
     if (playRound(playerSelection, computerSelection) == "playerWin") {
-        computerLife -= 1;
+        computerLife += 1;
         let msg = `You've won! ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}` +
                   ` beats ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}.` +
                   ` Player: ${playerLife} | Computer: ${computerLife}`;
         console.log(msg);
         alert(msg);
+        playerP.textContent = `Player: ${playerLife}`;
     } else if (playRound(playerSelection, computerSelection) == "computerWin") {
-        playerLife -= 1;
+        playerLife += 1;
         let msg = `You've lost! ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}` +
                   ` beats ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}.` + 
                   ` Player: ${playerLife} | Computer: ${computerLife}`;
         console.log(msg);
         alert(msg);
+        computerP.textContent = `Machine: ${computerLife}`;
     } else {
         let msg = `That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`
         console.log(msg);
@@ -45,7 +49,7 @@ function game() {
     }
   }
   // Declare WINNER of game
-  if (playerLife == 0) {
+  if (playerLife == 5) {
       console.log("The computer won, as you knew he would! - Game Over")
       alert("The computer won, as you knew he would! - Game Over");
   } else {
