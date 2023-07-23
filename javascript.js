da976d38fb6f06014aceab80460e6248431f91db
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-07-23 10:14:24 -0700

    Change play mode to button based
    First I moved  the lifePoint variable declaration to outside the game()
    so that they are more accessible and added some for the button.
    I also added a EventListener to the buttons, that run the game() by
    feeding it the weapon choice as an argument.
    I further adjusted the point counting function and the determination
    of the end of the game.
    I added the resetGame() to reset for a new start of the game.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+let playerLife = 0;
+let computerLife = 0;
+const playerP = document.querySelector("#playerP");
+const computerP = document.querySelector("#computerP");
+const choices = document.querySelector(".choices");
+
+choices.addEventListener('click', (e) => {
+  game(e.target.id);
+})
+
 function getComputerChoice() { 
   const choices = ['rock', 'paper', 'scissors']
   const computerChoice = choices[Math.floor(Math.random() *choices.length)];
@@ -14,46 +24,50 @@ function playRound(playerSelection, computerSelection) {
 }
 
 // Determine START and END of game and keep track of points
-function game() {
-  let playerLife = 0;
-  let computerLife = 0;
+function game(weapon) {
 
-  while (playerLife < 5 && computerLife < 5) {
-    const playerSelection = prompt('Choose your weapon! Pick from: Rock, Paper, or Scissors').toLowerCase();
-    const computerSelection = getComputerChoice();
-    const playerP = document.querySelector("#playerP");
-    const computerP = document.querySelector("#computerP");
+  const playerSelection = weapon;
+  const computerSelection = getComputerChoice();
 
-    playRound(playerSelection, computerSelection);
+  playRound(playerSelection, computerSelection);
 
-    if (playRound(playerSelection, computerSelection) == "playerWin") {
-        computerLife += 1;
-        let msg = `You've won! ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}` +
-                  ` beats ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}.` +
-                  ` Player: ${playerLife} | Computer: ${computerLife}`;
-        console.log(msg);
-        alert(msg);
-        playerP.textContent = `Player: ${playerLife}`;
-    } else if (playRound(playerSelection, computerSelection) == "computerWin") {
-        playerLife += 1;
-        let msg = `You've lost! ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}` +
-                  ` beats ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}.` + 
-                  ` Player: ${playerLife} | Computer: ${computerLife}`;
-        console.log(msg);
-        alert(msg);
-        computerP.textContent = `Machine: ${computerLife}`;
-    } else {
-        let msg = `That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`
-        console.log(msg);
-        alert(msg);
-    }
+  if (playRound(playerSelection, computerSelection) == "playerWin") {
+      computerLife += 1;
+      let msg = `You've won! ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}` +
+                ` beats ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}.` +
+                ` Player: ${playerLife} | Computer: ${computerLife}`;
+      console.log(msg);
+      alert(msg);
+      playerP.textContent = `Player: ${playerLife}`;
+  } else if (playRound(playerSelection, computerSelection) == "computerWin") {
+      playerLife += 1;
+      let msg = `You've lost! ${computerSelection.substring(0, 1).toUpperCase()+computerSelection.substring(1)}` +
+                ` beats ${playerSelection.substring(0, 1).toUpperCase()+playerSelection.substring(1)}.` + 
+                ` Player: ${playerLife} | Computer: ${computerLife}`;
+      console.log(msg);
+      alert(msg);
+      computerP.textContent = `Machine: ${computerLife}`;
+  } else {
+      let msg = `That was a tie, try again. Player: ${playerLife} | Computer: ${computerLife}`
+      console.log(msg);
+      alert(msg);
   }
   // Declare WINNER of game
-  if (playerLife == 5) {
-      console.log("The computer won, as you knew he would! - Game Over")
-      alert("The computer won, as you knew he would! - Game Over");
-  } else {
-      console.log("Hurray! Once again the human brain conquered the machine! - Game Over")
-      alert("Hurray! Once again the human brain conquered the machine! - Game Over")
+  if (playerLife == 5 || computerLife == 5) {
+    if (playerLife == 5) {
+        console.log("The computer won, as you knew he would! - Game Over")
+        alert("The computer won, as you knew he would! - Game Over");
+    } else {
+        console.log("Hurray! Once again the human brain conquered the machine! - Game Over")
+        alert("Hurray! Once again the human brain conquered the machine! - Game Over")
+    }
+  gameReset();
   }
+}
+
+function gameReset() {
+  playerLife = 0;
+  computerLife = 0;
+  playerP.textContent = `Player: 0`
+  computerP.textContent = `Machine: 0`
 }
